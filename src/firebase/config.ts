6faae1427baf44ef69526fa3fd4ab3b0b3d50368
PABLINO XAVIER BARBOZA RUIZ <xavierbarbozaruizdiaz@gmail.com>
2025-10-21 15:05:04 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,4 +2,11 @@
 // This file is intentionally left blank. 
 // The firebase config will be populated by the backend.
 // It is used to initialize the firebase app.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "conectado-13502288-23980",
+  "appId": "1:25457181031:web:e47200846d7c9cabb4efc2",
+  "apiKey": "AIzaSyBworqn4G5FzN90uxUuUyk9cnNsrkm7WwY",
+  "authDomain": "conectado-13502288-23980.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "25457181031"
+};
