e4930d5f108d884fa89867563f954ddfcd46233b
PABLINO XAVIER BARBOZA RUIZ <xavierbarbozaruizdiaz@gmail.com>
2025-10-22 14:40:53 +0000

    TPMS PUEDES ANALIZAR ESTO. NO MODIFIQUES NADA, SOLO EXPLICA A QUE SE REF

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,6 +7,7 @@ export const firebaseConfig = {
   "appId": "1:25457181031:web:e47200846d7c9cabb4efc2",
   "apiKey": "AIzaSyBworqn4G5FzN90uxUuUyk9cnNsrkm7WwY",
   "authDomain": "conectado-13502288-23980.firebaseapp.com",
+  "storageBucket": "conectado-13502288-23980.appspot.com",
   "measurementId": "",
   "messagingSenderId": "25457181031"
 };
